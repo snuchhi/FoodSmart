b0e4fec07253f5dbe12b65f070535c087a503d12
punam <punampal173@gmail.com>
2019-12-05 08:53:01 -0800

    added skeleton

---
+++ src/bmi.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class BMI extends Component {
+    
+render() {
+  return (
+    <section id="bmi">
+   
+    </section>
+  );
+}
+}
+export default BMI;
