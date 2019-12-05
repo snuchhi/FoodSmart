2f6933f221290552d656b10304ef53f8bbb1247a
punam <punampal173@gmail.com>
2019-12-05 09:42:24 -0800

    added App.js file to list the header content

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import React from 'react';
 function App() {
   return (
     <header id="header">
-       <p>hello</p>  
+        <div className="container-fluid">
+          <div id="logo" className="pull-left">
+            <h1><a href="#intro" className="scrollto">FoodSmart</a></h1>      
+          </div>
+        <nav id="nav-menu-container">
+          <ul className="nav-menu">
+            <li className="menu-active">
+              <a href="#intro">Home</a>
+            </li>
+            <li>
+              <a href="#about">About Us</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>  
+          </ul>
+        </nav>
+        </div>
     </header> 
    
   );
