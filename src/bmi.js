503098f8a72d071eae0009c37823ab232da26072
punam <punampal173@gmail.com>
2019-12-05 21:11:34 -0800

    added reat handler bmi.js

--- src/bmi.js
+++ src/bmi.js
@@ -1,8 +1,63 @@
 import React,{Component} from 'react';
 
 class BMI extends Component {
+
+    constructor(props) {
+
+        super(props);
+        this.state = { 
+          wt: '',
+          feet: '',
+          inch:'',
+          result: '0',
+          answer:'',
+          meaning:''
+        };
+
+        this.actionWtInputChange = this.actionWtInputChange.bind(this);
+        this.actionFeetInputChange = this.actionFeetInputChange.bind(this);
+        this.actionInchInputChange = this.actionInchInputChange.bind(this);
+        this.actionResult=this.actionResult.bind(this);    
+      }
+
+      actionWtInputChange = (e) => {
+        this.setState({ wt: Number(e.target.value)})
+      }
+      actionFeetInputChange = (e) => {
+        this.setState({ feet: Number(e.target.value)})
+      }
+      actionInchInputChange = (e) => {
+        this.setState({ inch: Number(e.target.value)})
+      }
     
+      actionResult = (e) => {
+
+        e.preventDefault();
+        let kg = this.state.wt * (0.4536); 
+        let f1= this.state.feet * 12; 
+        let inches = f1 + this.state.inch;
+        let totalh = inches * 0.0254; 
+        let x=kg / (totalh*totalh);
+
+        this.setState({ result: x.toFixed(1) });
+        this.setState({ answer: 'Your Calculated BMI: '+x.toFixed(1) }); 
+
+        if(x< 18.5){
+            this.setState({ meaning: 'Your BMI results indicate that you are underweight' });
+           
+        }else if(x > 18.5 && x< 25){
+            this.setState({ meaning: 'Your BMI results indicate that you are healthy' });
+          
+        }else if (x > 25){
+            this.setState({ meaning: 'Your BMI results indicate that you overweight' });
+           
+        }else {
+            this.setState({ meaning: '  Please Fill in everything correctly' }); 
+        }
+      }  
+
 render() {
+
   return (
     <section id="bmi">
         <div className="container">
