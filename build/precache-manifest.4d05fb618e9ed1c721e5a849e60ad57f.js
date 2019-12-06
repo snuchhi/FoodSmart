225d9d50a09e7aa3b8961e7b5b4ae52fc3b96c4a
Shruti Pattadakal <shruti@Shrutis-MacBook-Pro.local>
2019-12-06 01:02:53 -0800

    add build

---
+++ build/precache-manifest.4d05fb618e9ed1c721e5a849e60ad57f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29c7d28b9dc0d12bc04d1e8ffeea540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58b01aba84033acc6b0d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9870d064d7571e273f80",
+    "url": "/static/js/2.108ba811.chunk.js"
+  },
+  {
+    "revision": "58b01aba84033acc6b0d",
+    "url": "/static/js/main.324632ea.chunk.js"
+  },
+  {
+    "revision": "07d646d363509d700e55",
+    "url": "/static/js/runtime-main.30f5102d.js"
+  }
+]);
